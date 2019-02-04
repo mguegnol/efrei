702591b7202a1b0deffe8a16bb87e4239f250d5e
Maxence <maxence@MacBook-Pro-de-Maxence.local>
2019-02-04 11:43:52 +0100

    ou

--- NegException.java
+++
@@ -1,7 +0,0 @@
-package oui;
-
-public class NegException extends Exception {
-    public NegException() {
-        System.out.println("Erreur : Vous essayez de calculer la racine d'un nombre négatif !");
-    }
-}
